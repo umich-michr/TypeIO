0084a8da3d5014f1cad5051c496229ad32d9f005
Melih Gunal <gunalmel@med.umich.edu>
2016-08-18 15:41:33 -0400

    refs #3 - releasing

--- test/michr-typeahead.spec.js
+++ test/michr-typeahead.spec.js
@@ -21,115 +21,126 @@ describe('MICHR Typeahead Tests', function() {
     });
 
     it('Test no dataset and throwing exception', function() {
-        expect(function(){ $('#textTypeahead').newTypeAhead(options);}).toThrow(new Error('Please provide a data set'));
-
+         expect(function(){ $('#textTypeahead').typeIO(options);}).toThrow(new Error('Please provide a data set'));
     });
 
     it('Test with dataset, but no result container and throwing exception', function() {
-        expect(function(){ $('#textTypeahead').newTypeAhead(options, {});}).toThrow(new Error('Please provide results container'));
+        expect(function(){ $('#textTypeahead').typeIO(options, {});}).toThrow(new Error('Please provide results container'));
     });
 
     it('Test useDefaultMatcher but no source', function() {
-        var dataset = {
-            resultsContainer: '#divResultContainer',
-            useDefaultMatcher: true
+        var options = {
+            resultsContainer: '#divResultContainer'
         };
-        expect(function(){ $('#textTypeahead').newTypeAhead(options, dataset);}).toThrow(new Error('Please provide data source'));
+        expect(function(){ $('#textTypeahead').typeIO(options, {});}).toThrow(new Error('Please provide data source'));
     });
 
     it('Test useDefaultMatcher with invalid source', function() {
+        var options = {
+            resultsContainer: '#divResultContainer'
+        };
         var dataset = {
-            resultsContainer: '#divResultContainer',
-            useDefaultMatcher: true,
+            display: 'value',
             source: [{value: 'value', wrong: 'wrong'}]
         };
-        expect(function(){ $('#textTypeahead').newTypeAhead(options, dataset);}).toThrow(new Error('Invalid data provided: ' + dataset.source[0]));
+        expect(function(){ $('#textTypeahead').typeIO(options, dataset)}).toThrow(new Error('Invalid data provided: ' + dataset.source[0]));
     });
 
     it('Test useDefaultMatcher with valid source - match contains', function() {
+        var options = {
+            resultsContainer: '#divResultContainer'
+        };
         var dataset = {
-            resultsContainer: '#divResultContainer',
-            useDefaultMatcher: true,
+            display: 'text',
             source: [{value: 'value', text: 'text'}]
         };
-        $('#textTypeahead').newTypeAhead(options, dataset);
+        $('#textTypeahead').typeIO(options, dataset);
         dataset.source('ex', function(matches) {
             expect(matches).toEqual([{value: 'value', text: 'text'}]);
         });
     });
 
     it('Test useDefaultMatcher with valid source - match startsWith', function() {
-        var dataset = {
+        var options = {
             resultsContainer: '#divResultContainer',
-            useDefaultMatcher: true,
-            source: [{value: 'value', text: 'text'}],
             matchType: 'startsWith'
         };
-        $('#textTypeahead').newTypeAhead(options, dataset);
+        var dataset = {
+            display: 'text',
+            source: [{value: 'value', text: 'text'}]
+        };
+        $('#textTypeahead').typeIO(options, dataset);
         dataset.source('te', function(matches) {
             expect(matches).toEqual([{value: 'value', text: 'text'}]);
         });
     });
 
     it('Test useDefaultMatcher with valid source - match endsWith', function() {
-        var dataset = {
+        var options = {
             resultsContainer: '#divResultContainer',
-            useDefaultMatcher: true,
-            source: [{value: 'value', text: 'text'}],
             matchType: 'endsWith'
         };
-        $('#textTypeahead').newTypeAhead(options, dataset);
+        var dataset = {
+            display: 'text',
+            source: [{value: 'value', text: 'text'}]
+        };
+        $('#textTypeahead').typeIO(options, dataset);
         dataset.source('xt', function(matches) {
             expect(matches).toEqual([{value: 'value', text: 'text'}]);
         });
     });
 
     it('Test useDefaultMatcher with invalid initial result', function() {
-        var dataset = {
+        var options = {
             resultsContainer: '#divResultContainer',
-            useDefaultMatcher: true,
-            source: [{value: 'value', text: 'text'}],
             initialResults: [{wrong: 'wrong'}]
         };
-        expect(function(){ $('#textTypeahead').newTypeAhead(options, dataset);}).toThrow(new Error('Invalid data provided: ' + dataset.source[0]));
+
+        var dataset = {
+            source: [{value: 'value', text: 'text'}]
+        };
+        expect(function(){ $('#textTypeahead').typeIO(options, dataset);}).toThrow(new Error('Invalid data provided: ' + dataset.source[0]));
     });
 
     it('Test useDefaultMatcher with valid initial result - not single value', function() {
-        var dataset = {
+        var options = {
             resultsContainer: '#divResultContainer',
-            useDefaultMatcher: true,
-            source: [{value: 'value', text: 'text'}],
             initialResults: [{value: 'initValue', text: 'initText'}],
             name: 'name'
         };
-        $('#textTypeahead').newTypeAhead(options, dataset);
+        var dataset = {
+            display: 'text',
+            source: [{value: 'value', text: 'text'}]
+        };
+        $('#textTypeahead').typeIO(options, dataset);
 
-        expect($(dataset.resultsContainer).hasClass('tt-added-results')).toBe(true);
-        expect($(dataset.resultsContainer).html()).toContain('<ul data-tt-texttypeahead="" id="ulTypeaheadResults">');
-        expect($(dataset.resultsContainer).html()).toContain('<select aria-hidden="true" class="hide" multiple="" data-tt-texttypeahead="" id="selectTypeaheadFormResults" name="name">');
-        expect($(dataset.resultsContainer).html()).toContain('Remove');
-        expect($(dataset.resultsContainer).html()).toContain('<li id="liTypeaheadSelected-initValue"><span class="display-text">initText</span>');
-        expect($(dataset.resultsContainer).html()).toContain('<option selected="" value="initValue"></option>');
+        expect($(options.resultsContainer).hasClass('tt-added-results')).toBe(true);
+        expect($(options.resultsContainer).html()).toContain('<ul data-tt-texttypeahead="" id="ulTypeaheadResults">');
+        expect($(options.resultsContainer).html()).toContain('<select aria-hidden="true" class="hide" multiple="" data-tt-texttypeahead="" id="selectTypeaheadFormResults" name="name">');
+        expect($(options.resultsContainer).html()).toContain('Remove');
+        expect($(options.resultsContainer).html()).toContain('<li id="liTypeaheadSelected-initValue"><span class="display-text">initText</span>');
+        expect($(options.resultsContainer).html()).toContain('<option selected="" value="initValue"></option>');
     });
 
     it('Test useDefaultMatcher with valid initial result - single value', function() {
-        var dataset = {
+        var options = {
             resultsContainer: '#divResultContainer',
-            useDefaultMatcher: true,
-            source: [{value: 'value', text: 'text'}],
-            initialResults: [{value: 'initValue', text: 'initText'}],
             name: 'name',
-            singleValue: true
+            mode: 'single-select',
+            initialResults: [{value: 'initValue', text: 'initText'}]
         };
-        $('#textTypeahead').newTypeAhead(options, dataset);
+        var dataset = {
+            display: 'text',
+            source: [{value: 'value', text: 'text'}]
+        };
+        $('#textTypeahead').typeIO(options, dataset);
 
-        expect($(dataset.resultsContainer).hasClass('tt-added-results')).toBe(true);
+        expect($(options.resultsContainer).hasClass('tt-added-results')).toBe(true);
         expect($('#textTypeahead').hasClass('hide')).toBe(true);
-        console.log($(dataset.resultsContainer));
-        expect($(dataset.resultsContainer).html()).toContain('<ul data-tt-texttypeahead="" id="ulTypeaheadResults">');
-        expect($(dataset.resultsContainer).html()).toContain('<select aria-hidden="true" class="hide" multiple="" data-tt-texttypeahead="" id="selectTypeaheadFormResults" name="name">');
-        expect($(dataset.resultsContainer).html()).toContain('Change');
-        expect($(dataset.resultsContainer).html()).toContain('<li id="liTypeaheadSelected-initValue"><span class="display-text">initText</span>');
-        expect($(dataset.resultsContainer).html()).toContain('<option selected="" value="initValue"></option>');
+        expect($(options.resultsContainer).html()).toContain('<ul data-tt-texttypeahead="" id="ulTypeaheadResults">');
+        expect($(options.resultsContainer).html()).toContain('<select aria-hidden="true" class="hide" multiple="" data-tt-texttypeahead="" id="selectTypeaheadFormResults" name="name">');
+        expect($(options.resultsContainer).html()).toContain('Change');
+        expect($(options.resultsContainer).html()).toContain('<li id="liTypeaheadSelected-initValue"><span class="display-text">initText</span>');
+        expect($(options.resultsContainer).html()).toContain('<option selected="" value="initValue"></option>');
     });
 });
